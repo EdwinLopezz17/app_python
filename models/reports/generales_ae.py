738bed6a2f9f3a8d4323bc92a3d1edb531181984
unknown <edwinabdias@gmail.com>
2026-09-01 17:21:50 -0500

    feat: refactorizacion de genericos

---
+++ models/reports/generales_ae.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class GeneralesAE:
+    db: str
+    cuenta_acceso: str
+    host_conexion: str
+    terminal: str
+    fecha_cierre_sesion: str
+    elemento_consultado: str
+    cuenta_usuario: str
+    fecha_accion: str
+    codigo_accion: str
+    jefe_chapter_lead: str
+    val_cuenta_acceso: str
+    usuario_utilizado: str
+    val_usuario_utilizado: str
+    usuario_corresponde: str
+    val_usuario_corrsponde: str
